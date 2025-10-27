7a35adb5d83660670f60a8f378973d8ab36873fb
wzawza02 <s1140335@dmhs.kh.edu.tw>
2025-10-27 16:16:00 +0800

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int age = 16;
+    double height = 177;
+    std::string name = "王梓安";
+    bool is_student = false;
+    std::cout<< "姓名: " << name << std::endl; 
+    std::cout<< "身高: " << height << std::endl; 
+    std::cout<< "年齡: " << age << std::endl; 
+    std::cout<< "是否為學生: " << is_student << std::endl; 
+    
+    
+    return 0;
+}
