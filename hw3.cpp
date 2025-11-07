239bbaf0c3b77a5f9609bc43cf8ec5d9f8215077
wzawza02 <s1140335@dmhs.kh.edu.tw>
2025-11-07 11:37:16 +0800

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"台股大跌" << std::endl;
+    std::cout<<"台 股 大 跌" << std::endl;
+    std::cout<<"台\t股\t大\t跌" << std::endl;
+    std::cout<<"台\n股\n大\n跌" << std::endl;
+    std::cout<<"\"台股大跌\"" << std::endl;
+    std::cout<<"\\台股大跌\\" << std::endl;
+    \
+    return 0;
+}
