1d73a587a956399d10bf267052f4bd3e5ce3ea34
wzawza02 <s1140335@dmhs.kh.edu.tw>
2025-12-15 16:19:58 +0800

    Add files via upload

--- hw12.cpp
+++ hw12.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 
 int main()
 {
-    int subject = 5;
-    int grade[5] = {95,10,30,80,60};
-    int sum = 0;
-    for(int i=0; i<5; i++) {
-        sum = sum + grade[i];
+     int score[5][3] = {
+        {80, 90, 85},
+        {70, 75, 78},
+        {88, 92, 95},
+        {60, 65, 70},
+        {90, 93, 89}
+    };
+    
+    string name[5] = {
+        "Amy",
+        "Ben",
+        "Cindy",
+        "David",
+        "Eva"
+    };
+    for (int i=0; i<5; i++) {
+        int sum=0;
+        cout << name[i] << " çš„æˆç¸¾: ";
+        for (int j=0;j<3;j++) {
+            cout << score[i][j] << " ";
+            sum = sum + score[i][j];
+        }
+        cout<< "æœ€é«˜åˆ†æ•¸: " << sum << endl;
     }
-    int average = sum/subject;
-    cout << "¬q¦Ò¥­§¡¬°: " << average;
     return 0;
 }
